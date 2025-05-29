90f4f6a7d7d30ee1bdcf65c8fc7c7d48c31a2fce
DanielPiekarski <danielpiek2005@gmail.com>
2025-05-29 14:19:57 +0200

    Fotos y mapa

---
+++ WebPolideportivo/WebPolideportivo/Models/PosicionPista.cs
@@ -0,0 +1,10 @@
+﻿namespace WebPolideportivo.Models;
+
+public class PosicionPista
+{
+    public int Id { get; set; }
+    public string Top { get; set; }
+    public string Left { get; set; }
+    public string Width { get; set; }
+    public string Height { get; set; }
+}
--- WebPolideportivo/WebPolideportivo/obj/Debug/net8.0/WebPolideportivo.AssemblyInfo.cs
+++ WebPolideportivo/WebPolideportivo/obj/Debug/net8.0/WebPolideportivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4fc5fd73a13991bc8412e6448ae1c1779debc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146e72f4bb392c2f705a983b782a268e284595d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
