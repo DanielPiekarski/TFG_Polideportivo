347df8d7d2be1c433e95cfa3e4cd1d6b3d3b62ec
DanielPiekarski <danielpiek2005@gmail.com>
2025-06-05 15:18:58 +0200

    Imágenes nuevas

--- WebPolideportivo/WebPolideportivo/obj/Debug/net8.0/WebPolideportivo.AssemblyInfo.cs
+++ WebPolideportivo/WebPolideportivo/obj/Debug/net8.0/WebPolideportivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146e72f4bb392c2f705a983b782a268e284595d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d9ca62b0e177e49182f0987176456820095bb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPolideportivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
